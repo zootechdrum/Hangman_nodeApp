f395958e8cb7094c18b6ba4876690656984cc847
Cesar Gomez <zootechdrum@gmail.com>
2019-12-04 22:51:01 -0800

    added Word file and started builing letter constructor function

---
+++ src/Letter.js
@@ -0,0 +1,17 @@
+function Letter (letter) {
+    this.letter = letter;
+    this.attemptLetter = false;
+
+    this.guessedLetter = function(letter) {
+        if(this.letter === false ) {
+            return letter
+        }else {
+            return "_"
+        }
+    }
+
+
+}
+
+const l = new Letter()
+console.log(l)
